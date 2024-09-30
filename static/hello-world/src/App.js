04bf5087e8b455e33feb3daea76b73a811c36176
Don Alcántara <gabriel.alcantara@quabu.eu>
2024-09-30 09:05:25 +0200

    changes required to test

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { invoke } from "@forge/bridge";
+// import { invoke } from "@forge/bridge";
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    invoke("getText", { example: "my-invoke-variable" }).then(setData);
+    // invoke("getText", { example: "my-invoke-variable" }).then(setData);
+    console.log("Hello, world!");
   }, []);
 
-  return <div>{data ? data : "Hi Gustavo..."}</div>;
+  console.log("Hello, world!");
+
+  return <div>{data ? data : "Hi TSM..."}</div>;
 }
 
 export default App;
